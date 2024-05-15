154e329f48e6c253b1fbe970cac1d870311c9f54
qkgallave <142886936+qkgallave@users.noreply.github.com>
2024-05-15 20:03:31 +0800

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+
+# Load the CNN model
+model = load_model('cnn_model.h5')
+
+# Function to preprocess the image
+def preprocess_image(image):
+    resized_image = image.resize((150, 150))  # Adjust the size according to your model's input size
+    normalized_image = np.array(resized_image) / 255.0
+    preprocessed_image = np.expand_dims(normalized_image, axis=0)
+    return preprocessed_image
+
+# Display title and image
+st.title("Model Deployment on the Cloud")
+st.image("weather.jpg", width=400)
+st.text("Using the Weather Dataset to predict from an uploaded image.")
+
+# Upload image
+uploaded_image = st.file_uploader("Choose an image that can be classified as rainy, sunny, shine, or cloudy: ", type=["jpg", "png", "jpeg"])
+
+if uploaded_image is not None:
+    # Display the uploaded image
+    image = Image.open(uploaded_image)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    # Preprocess the image
+    preprocessed_image = preprocess_image(image)
+
+    # Make prediction
+    prediction = model.predict(preprocessed_image)
+
+    # Define categories
+    categories = ['shine', 'sunset', 'rainy', 'cloudy']
+
+    # Create a dictionary to map indices to categories
+    category_mapping = {i: category for i, category in enumerate(categories)}
+
+    # Display prediction
+    max_index = np.argmax(prediction)
+    predicted_category = category_mapping[max_index]
+    st.write("Prediction Category:", predicted_category)
+    st.write("Prediction Probability:", prediction[0][max_index])
